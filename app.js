d04e90bb144acaeedbd641d6343fe04296712297
Matt Copperwaite <mattcopp+git@gmail.com>
2013-12-23 19:30:45 +0000

    Working account handling form

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ module.exports = {
     "name": "membership",
     "app": function(db, site, config) {
         var app = express();
+        
+        app.set('views', __dirname + "/views");
+        app.use(express.csrf());
+        app.use(function (req, res, next) {
+            res.locals.token = req.csrfToken();
+            next();
+        });
 
         app.get('/', function(req, res){
-          res.render("base");
+            if (req.session.email) {
+                res.render("membership");
+            }
+            else {
+                res.redirect("/"); // should probably be handled better or elsewhere
+            }
         });
         return app;
     }
