b8c9ec0a91be5396383f1f6aa03f9cd5a266cecf
Matt Copperwaite <mattcopp+git@gmail.com>
2014-01-02 19:34:50 +0000

    Got database writing working

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var _ = require("underscore");
+
 module.exports = {
     "title": "Membership",
     "name": "membership",
-    "app": function(db, site, config) {
+    "app": function(config, db, site) {
         var app = express();
+        
+        _.extend(app.locals, site.locals);
         
         app.set('views', __dirname + "/views");
         app.use(express.csrf());
@@ -17,7 +21,33 @@ module.exports = {
                 res.render("membership");
             }
             else {
-                res.redirect("/"); // should probably be handled better or elsewhere
+                res.redirect("/"); // could probably be handled better or elsewhere
+            }
+        });
+        
+        app.post("/", function (req, res) {
+            if (res.locals.user) {
+                // update user
+                var user = res.locals.user;
+                user.name = req.body.name;
+                user.address = req.body.address;
+                user.card_id = req.body.card_id;
+                
+                user.save(function (err, user) {
+                    res.render("membership", {user: user});
+                });
+            }
+            else {
+                // create user
+                res.locals.User.create({
+                    email: req.session.email,
+                    name: req.body.name,
+                    address: req.body.address,
+                    card_id: req.body.card_id
+                },
+                function (err, user) {
+                    res.render("membership", {user: user});
+                });
             }
         });
         return app;
