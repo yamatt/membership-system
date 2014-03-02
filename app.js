58c2938a5ce0b869b3789a6ac2e9669bf6c19cf3
Matt Copperwaite <mattcopp+git@gmail.com>
2014-03-02 15:13:20 +0000

    changes so that payment minimum is now in the configuration file

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express'),
     _ = require("underscore"),
     GoCardless = require('gocardless');
 
-var MIN_SUBSCRIPTION = 5.00; // TODO: configuration file
-
 module.exports = {
     "title": "Membership",
     "name": "membership",
@@ -73,7 +71,7 @@ module.exports = {
         app.post("/", function (req, res) {
             var user = res.locals.user;
             if ((user) && (req.body.subscribe == "Become Member")) {
-                if (parseFloat(req.body.subscription) >= MIN_SUBSCRIPTION) {
+                if (parseFloat(req.body.subscription) >= config.gocardless.minimum) {
                     var url = gc.subscription.newUrl({
                       amount: req.body.subscription,
                       interval_length: '1',
@@ -87,7 +85,7 @@ module.exports = {
                     res.redirect(url);
                 }
                 else {
-                    res.locals.flash("warning", "Subscription failed.", "Please enter a value greater than £" + MIN_SUBSCRIPTION + "."); // TODO: configurable currency symbol
+                    res.locals.flash("warning", "Subscription failed.", "Please enter a value greater than £" + config.gocardless.minimum + "."); // TODO: configurable currency symbol
                 }
             }
             if ((user) && (req.body.subscribe == "Cancel Payment") && (user.gc_subscription)) {
